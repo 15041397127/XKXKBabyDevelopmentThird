868b3cb39d3ebf780d0bca3cfb7511345efff89b
ZhangXu <zhangxu@zxdemacbook-pro.local>
2016-07-04 09:32:39 +0800

    Initial Commit

---
+++ XKXKBabyDevelopmentThird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XKXKBabyDevelopmentThird
+//
+//  Created by 张旭 on 16/7/4.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XKXKBabyDevelopmentThird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XKXKBabyDevelopmentThird
+//
+//  Created by 张旭 on 16/7/4.
+//  Copyright © 2016年 zhangXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
